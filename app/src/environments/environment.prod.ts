e33fcdd4b2b390083ba5aacbe9b3136e4a9765bc
Sushant Sharma <ssushantlb08@gmail.com>
2022-09-08 23:34:08 +0530

    changes

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-signup-242e1',
+    appId: '1:1047340293795:web:a17c07241763e8480c6916',
+    storageBucket: 'angular-signup-242e1.appspot.com',
+    apiKey: 'AIzaSyDoa4pjnQV3SL3CFDMlqU0Li_TRUnThOmU',
+    authDomain: 'angular-signup-242e1.firebaseapp.com',
+    messagingSenderId: '1047340293795',
+  },
+  production: true
+};
