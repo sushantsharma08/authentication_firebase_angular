965fc0447cd1929236ec06c70fa7e42a80431b80
Sushant Sharma <ssushantlb08@gmail.com>
2022-09-10 01:25:58 +0530

    home page ui and code refactor

--- app/src/app/services/authentication.service.ts
+++ app/src/app/services/authentication.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { from } from 'rxjs';
+import {   Auth,
+  signInWithEmailAndPassword,
+  authState,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  UserInfo,
+  UserCredential,} from '@angular/fire/auth';
+  import { concatMap, from, Observable, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +15,10 @@ export class AuthenticationService {
 
   constructor(private auth:Auth) { }
 
-  login(username:string, password:string){
-   return from(signInWithEmailAndPassword(this.auth, username,password))
+  // login(username:string, password:string){
+  //  return from(signInWithEmailAndPassword(this.auth, username,password))
+  // }
+  login(email: string, password: string): Observable<any> {
+    return from(signInWithEmailAndPassword(this.auth, email, password));
   }
 }
